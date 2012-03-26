365744a7a4bd24723fba9d7c39f400d508e5f6be
Andreas Butti <andreasbutti@gmail.com>
2012-03-26 21:32:15 +0200

    Text edit DLG

--- BJavalibs/BJavalibs/src/butti/javalibs/gui/TextEditDialog.java
+++ BJavalibs/BJavalibs/src/butti/javalibs/gui/TextEditDialog.java
@@ -1,9 +1,11 @@
 package butti.javalibs.gui;
 
+import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JWindow;
 
@@ -23,14 +25,14 @@ public class TextEditDialog extends BDialog {
 
 	public TextEditDialog(JWindow win, String description) {
 		super(win);
-		
+
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setModal(true);
 
-		gbm = new GridBagManager(this, true);
+		gbm = new GridBagManager(this);
 
 		gbm.setX(0).setWeightY(0).setWidth(3).setY(0).setComp(lbDescription);
-		gbm.setX(0).setWeightY(1).setWidth(3).setY(0).setComp(txtText);
+		gbm.setX(0).setWeightY(0).setWidth(3).setY(1).setComp(txtText);
 
 		JButton btCancel = ButtonFactory.createButton("Abbrechne", false);
 		btCancel.addActionListener(new ActionListener() {
@@ -40,9 +42,12 @@ public class TextEditDialog extends BDialog {
 				dispose();
 			}
 		});
-		gbm.setX(1).setY(2).setWeightY(0).setWeightX(0).setComp(btCancel);
 
-		JButton btOK = ButtonFactory.createButton("Speichern", false);
+		gbm.setX(0).setY(2).setComp(new JLabel());// dummy
+
+		gbm.setX(1).setY(2).setWeightY(0).setFill(GridBagConstraints.NONE).setWeightX(0).setComp(btCancel);
+
+		JButton btOK = ButtonFactory.createButton("Speichern", true);
 		btOK.addActionListener(new ActionListener() {
 
 			@Override
@@ -51,10 +56,27 @@ public class TextEditDialog extends BDialog {
 				dispose();
 			}
 		});
-		gbm.setX(1).setY(2).setWeightY(0).setWeightX(0).setComp(btOK);
+		gbm.setX(2).setY(2).setWeightY(0).setFill(GridBagConstraints.NONE).setWeightX(0).setComp(btOK);
+
+		pack();
+	}
+
+	/**
+	 * @return The inserted text, or <code>null</code> if canceled
+	 */
+	public String getText() {
+		return text;
 	}
 
 	public void setDescription(String description) {
 		lbDescription.setText(description);
+	}
+
+	public static void main(String[] args) {
+		TextEditDialog dlg = new TextEditDialog("test");
+		dlg.setTitle("Bla");
+		dlg.setVisible(true);
+
+		System.exit(0);
 	}
 }
