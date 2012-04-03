74c29d5ead13e31e3729afc0bf3126271376fb23
Andreas Butti <andreasbutti@gmail.com>
2012-04-03 21:28:08 +0200

    Added color configuration

---
+++ BJavalibs/BJavalibs/src/butti/javalibs/util/ColorConstants.java
@@ -0,0 +1,13 @@
+package butti.javalibs.util;
+
+import java.awt.Color;
+
+/**
+ * Constants colors
+ * 
+ * @author Andreas Butti
+ */
+public class ColorConstants {
+	public static final Color ERROR_COLOR = new Color(0xFFAAAA);
+
+}
